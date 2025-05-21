f4a1cdd1692dfcbb68927952324fb74f2a239bf4
ducktrshessami <cal.tf2.3@gmail.com>
2025-05-21 14:45:53 -0700

    feat: base structure

---
+++ src/error.ts
@@ -0,0 +1,6 @@
+export class ApplicationCommandOptionResolutionError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = this.constructor.name;
+    }
+}
